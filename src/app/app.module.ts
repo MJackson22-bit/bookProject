0691ca08628f533793645272ccedde8e2f21bd4b
MJackson22-bit <jm5444859@gmail.com>
2021-11-13 01:44:20 -0600

    Frontend
    Menu y vista de card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './component/book/book.component';
+import { MenuComponent } from './component/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
