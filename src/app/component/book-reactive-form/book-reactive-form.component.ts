3327a2e42b17c7f185c00cad63a31d525b6e1b95
MJackson22-bit <jm5444859@gmail.com>
2021-11-20 00:45:47 -0600

    Formulario reactivo

--- src/app/component/book-reactive-form/book-reactive-form.component.ts
+++ src/app/component/book-reactive-form/book-reactive-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Book } from '../../models/book';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-reactive-form',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookReactiveFormComponent implements OnInit {
 
-  constructor() { }
+  bookForm = this.fb.group({
+    tittle: ['', Validators.required],
+    description: ['', Validators.required],
+    image: ['', Validators.required],
+    publication: ['', Validators.required],
+    author: ['', Validators.required],
+    matter: ['', Validators.required],
+    synopsis: ['', Validators.required]
+  });
+
+  @Output()
+  addBookEventFromParent = new EventEmitter<Book>();
+
+  constructor(private fb: FormBuilder){
+
+  }
 
   ngOnInit(): void {
+  }
+  onSubmit(){
+    this.addBookEventFromParent.emit(this.bookForm.value);
   }
 
 }
